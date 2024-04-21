9087e7a1ff54d618fdfe94cb6c281deb92c15191
Shivam Dubey <108024596+Shivam00002@users.noreply.github.com>
2024-04-21 22:29:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './components/Home';
 function App() {
   return (
     <>
+
       <Home />
       <Toaster toastOptions={{ position: 'top-center', style: { fontSize: "20px" } }} />
     </>
